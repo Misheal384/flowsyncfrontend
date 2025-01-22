79a6f618e49055b7b7357a7f79e5a8d12b18e64c
Misheal384 <eversonmisheal2@gmail.com>
2025-01-22 11:58:11 +0000

    added standupdetails page

--- src/routes.tsx
+++ src/routes.tsx
@@ -8,12 +8,13 @@ import RemoveTeam from './components/pages/RemoveTeam';
 import RemoveMember from './components/pages/RemoveMember';
 import ConfigureStandup from './components/pages/ConfigureStandupQuestions';
 // import SetUniqueTeamReminder from './components/pages/SetUniqueTeamSchedules';
-import SetReminderTimes from './components/pages/SetReminderTImes';
+import SetReminderTimes from './components/pages/SetReminderTimes';
 import StandupPage from './components/pages/StandupPage';
 import StandupCollection from './components/pages/StandupCollection';
 import NotFoundPage from './components/pages/NotFoundPage';
 import Reporting from './components/pages/Reporting';
 import Reports from './components/pages/Reports';
+import StandupDetailsPage from './components/pages/StandupDetailsPage';
 
 const router = createBrowserRouter([
   { path: '/', element: <HomePage /> },
@@ -27,12 +28,14 @@ const router = createBrowserRouter([
   { path: '/set-reminder-times', element: <SetReminderTimes /> },
   { path: '/configure-standup-questions', element: <ConfigureStandup /> },
   { path: '/standup-collection', element: <StandupCollection /> },
+  
   {path: '/Reporting', element: <Reporting />},
   //need to make huge adjustments
   //changes within here to be done
   { path: '/create-team', element: <CreateTeamPage /> },
   { path: '/teams/:teamId/members', element: <AddMemberPage /> },
   { path: '/teams/:teamId/members/:memberId/standup', element: <StandupPage /> },
+  { path: '/standup-collection/:id', element: <StandupDetailsPage /> }, // Add this route
   { path: '/reports', element: <Reports /> },
   { path: '*', element: <NotFoundPage /> },
 ]);
